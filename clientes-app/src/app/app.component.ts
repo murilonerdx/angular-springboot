79f1742a8ae41669eace254b5e086c63a0570297
n30config <mu-silva@outlook.com>
2021-01-27 12:49:19 -0300

    Adicionando botão e configurando Scripts

--- clientes-app/src/app/app.component.ts
+++ clientes-app/src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit} from '@angular/core';
+import * as jQuery from 'jquery'
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'clientes-app';
+  href: string | undefined;
+
+  ngAfterViewInit(){
+    /*!
+    * Start Bootstrap - SB Admin v6.0.0 (https://startbootstrap.com/templates/sb-admin)
+    * Copyright 2013-2020 Start Bootstrap
+    * Licensed under MIT (https://github.com/BlackrockDigital/startbootstrap-sb-admin/blob/master/LICENSE)
+    */
+   (($) => {
+    "use strict";
+
+    // Add active state to sidbar nav links
+    var path = window.location.href; // because the 'href' property of the DOM element is the absolute path
+        $("#layoutSidenav_nav .sb-sidenav a.nav-link").each(() => {
+            if (this.href === path) {
+                $(this).addClass("active");
+            }
+        });
+
+    // Toggle the side navigation
+    $("#sidebarToggle").on("click", function(e: { preventDefault: () => void; }) {
+        e.preventDefault();
+        $("body").toggleClass("sb-sidenav-toggled");
+    });
+})(jQuery);
+
+  }
 }

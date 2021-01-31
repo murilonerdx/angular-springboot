ba8ab3cecf58b8b50ed57ed4ef00a5489254437a
n30config <mu-silva@outlook.com>
2021-01-31 15:16:04 -0300

    Criando servicoPrestado

--- backend/src/main/java/com/springbootangular/main/controllers/ServicoPrestadoController.java
+++ backend/src/main/java/com/springbootangular/main/controllers/ServicoPrestadoController.java
@@ -9,14 +9,12 @@ import com.springbootangular.main.model.repository.ServicoPrestadoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 @RestController
 @Controller
@@ -54,5 +52,12 @@ public class ServicoPrestadoController {
         servicoPrestado.setValor(bigDecimalConverter.converter(dto.getPreco()));
         return servicoPrestadoRepository.save(servicoPrestado);
 
+    }
+
+    @GetMapping()
+    public List<ServicoPrestado> pesquisar(
+            @RequestParam(value = "nome", required = false) String nome, @RequestParam(value="mes", required = false) Integer mes
+    ){
+        return servicoPrestadoRepository.findByNomeClienteAndMes("%" + nome + "%", mes);
     }
 }
--- backend/src/main/java/com/springbootangular/main/model/repository/ServicoPrestadoRepository.java
+++ backend/src/main/java/com/springbootangular/main/model/repository/ServicoPrestadoRepository.java
@@ -2,8 +2,15 @@ package com.springbootangular.main.model.repository;
 
 import com.springbootangular.main.model.entity.ServicoPrestado;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ServicoPrestadoRepository extends JpaRepository<ServicoPrestado,Integer> {
+
+    @Query("select s from ServicoPrestado s join s.cliente c where upper(c.nome) like upper (:nome) and MONTH(s.data) =:mes")
+    List<ServicoPrestado> findByNomeClienteAndMes(@Param("nome") String nome, @Param("mes") Integer mes);
 }

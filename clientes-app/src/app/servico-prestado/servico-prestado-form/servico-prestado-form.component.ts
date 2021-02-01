787ca8638ba08ccc8b7096cd614c7657be7b5db1
n30config <mu-silva@outlook.com>
2021-02-01 15:09:04 -0300

    Criando componentes de lista e formulario de servico prestado

--- clientes-app/src/app/servico-prestado/servico-prestado-form/servico-prestado-form.component.ts
+++ clientes-app/src/app/servico-prestado/servico-prestado-form/servico-prestado-form.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Cliente } from 'src/app/clientes/clientes';
+import {ClientesService} from '../../clientes.service'
+import { ServicoPrestado } from '../servicoPrestado';
 @Component({
   selector: 'app-servico-prestado-form',
   templateUrl: './servico-prestado-form.component.html',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServicoPrestadoFormComponent implements OnInit {
 
-  constructor() { }
+
+  clientes: Cliente[] = []
+  servico: ServicoPrestado
+
+  constructor(private clienteService: ClientesService) {
+    this.servico = new ServicoPrestado();
+   }
 
   ngOnInit(): void {
+    this.clienteService.getClientes().subscribe(response => this.clientes = response);
+  }
+
+  onSubmit(){
+    console.log(this.servico)
   }
 
 }

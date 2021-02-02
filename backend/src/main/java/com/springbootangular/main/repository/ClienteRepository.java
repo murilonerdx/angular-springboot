9a93cb89815d4b294772f550872b719e89d7f5ae
n30config <mu-silva@outlook.com>
2021-02-02 17:40:34 -0300

    delete repository

--- backend/src/main/java/com/springbootangular/main/repository/ClienteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.springbootangular.main.repository;
-
-import com.springbootangular.main.model.Cliente;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ClienteRepository extends JpaRepository<Cliente,Integer> {
-}
